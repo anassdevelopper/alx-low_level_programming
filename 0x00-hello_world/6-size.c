999d74e0f5aa2abf60849ecfd77802f801d0a8e1
anass <anass.work22@gmail.com>
2023-03-16 15:25:32 -0700

    cor4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	float f;
 
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf("size of a long: %ld byte(s)\n", sizeof(i));
+	printf("size of a int: %ld byte(s)\n", sizeof(i));
 	printf("size of a long: %ld byte(s)\n", sizeof(li));
 	printf("size of a long long: %ld byte(s)\n", sizeof(lli));
 	printf("size of a float: %ld byte(s)\n", sizeof(f));
