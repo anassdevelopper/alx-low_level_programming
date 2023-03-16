963a426fbcdef495d7978fe024e0ed9a5157284e
anass <anass.work22@gmail.com>
2023-03-16 15:36:44 -0700

    cor4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,8 +4,8 @@
  * main - Entry point
  *
  * Description: Prints the sizes of various data types in bytes
- *
- * Return: Always 0 (Success)
+ * this program prints
+ * Return: 0
  */
 int main(void)
 {
@@ -17,8 +17,8 @@ int main(void)
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
