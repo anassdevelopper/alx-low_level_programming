b33d8b26783788f73ce7e94640c655ea85449fcd
anass <anass.work22@gmail.com>
2023-03-16 15:20:11 -0700

    cor4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the sizes of various data types in bytes
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
+    char c;
+    int i;
+    long li;
+    long long lli;
+    float f;
 
-	printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf("size of a long: %ld byte(s)\n", sizeof(i));
-	printf("size of a long: %ld byte(s)\n", sizeof(li));
-	printf("size of a long long: %ld byte(s)\n", sizeof(lli));
-	printf("size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
+    printf("size of a char: %ld byte(s)\n", sizeof(c));
+    printf("size of an int: %ld byte(s)\n", sizeof(i));
+    printf("size of a long: %ld byte(s)\n", sizeof(li));
+    printf("size of a long long: %ld byte(s)\n", sizeof(lli));
+    printf("size of a float: %ld byte(s)\n", sizeof(f));
+
+    return (0);
 }
