a34f499f65ecf245031f9aeed6a97f9b71d441f0
anass <anass.work22@gmail.com>
2023-03-16 14:53:09 -0700

    cor4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+* main
+* 
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
