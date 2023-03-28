68a35b5e71a02bb9073da8b511370dc363956f08
anass <anass.work22@gmail.com>
2023-03-27 17:04:09 -0700

    k

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen -returns the length of a string
+ * @s: string
+ * Return: returns an integer
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[index] != '\0'; i++)
+		;
+	return (i);
+}
