2fad05e0ff4399608540def5fbb6176a4dda15ba
anass <anass.work22@gmail.com>
2023-04-03 11:23:31 -0700

    k

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,19 @@
-/**
- *
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i = 0;
+#include "main.h"
 
-	while (i < n)
-	{
-		*(s + i) = b;
-		i++;
-	}
+/**
+ * set_chars - sets the first n characters of an array to a given value
+ * @s: pointer to the array
+ * @b: the value to set
+ * @n: number of characters to set
+ *
+ * Return: pointer to the modified array
+ */
+char *set_chars(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
 	return (s);
 }
